6bad85fc0941a3c7edeee8c43cb8189825e4515c
GabrielGrenier <gabriel.braun-grenier@hotmail.com>
2018-04-25 11:14:28 -0400

    valideProjet avec boucle for

--- modele/script/validationScript.php
+++ modele/script/validationScript.php
@@ -1,30 +1,15 @@
 <script>
 function validationProjet() {
-    $form = document.getElementById("formAddPro");
-    $nom = document.getElementById("addNomPro");
-    $num = document.getElementById("addNumPro");
-    //si l'un des champs est valide
-    if($nom.value!=""){$nom.style.borderColor = "initial";}
-    if($num.value!=""){$num.style.borderColor = "initial";}
-    //si l'un des champs est vide
-    if($nom.value==""){$nom.style.borderColor = "red";}
-    if($num.value==""){$num.style.borderColor = "red";}
-    //si les deux champs sont valide
-    if($nom.value!=""&&$num.value!=""){$form.submit();}
-    //refaire le code avec boucle
-    /*
-    window.onload = function () {
-  var form = document.getElementById('theForm');
-  form.button.onclick = function (){
-    for(var i=0; i < form.elements.length; i++){
-      if(form.elements[i].value === '' && form.elements[i].hasAttribute('required')){
-        alert('There are some required fields!');
-        return false;
-      }
+    var form = document.getElementById("formAddPro");
+
+    var valide = true;
+    for(i =0; i < form.length-1; i++){ //-1 a cause du td action
+        if(form.elements[i].value==""){
+            form.elements[i].style.borderColor="red";
+            form.elements[i].focus();
+            valide = false;}
+        if(form.elements[i].value!=""){form.elements[i].style.borderColor="initial";}
     }
-    form.submit();
-  }; 
-};
-*/
+    if(valide){form.submit();}
 }
 </script>
