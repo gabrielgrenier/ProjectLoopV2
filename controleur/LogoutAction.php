6ecfa2c0a4cd7e1f1db8f036c70619d90f21314a
GabrielGrenier <gabriel.braun-grenier@hotmail.com>
2018-04-25 11:41:31 -0400

    ajout de nouveau unset pour logout

--- controleur/LogoutAction.php
+++ controleur/LogoutAction.php
@@ -4,6 +4,9 @@ class LogoutAction implements Action {
 	public function execute(){
 		UNSET($_SESSION["connecte"]);
 		UNSET($_SESSION["current_user"]);
+        UNSET($_SESSION["current_email"]);
+        UNSET($_SESSION["current_projet"]);
+        UNSET($_SESSION["numProjet"]);
 		session_destroy();
 		return "default";
 	}
