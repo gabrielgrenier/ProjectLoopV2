9e0d6a40dea902ab4b65de62d390efb6f2a546ff
GabrielGrenier <gabriel.braun-grenier@hotmail.com>
2018-04-27 10:17:47 -0400

    validation dans edit tache

--- controleur/EditTacheAction.php
+++ controleur/EditTacheAction.php
@@ -3,7 +3,14 @@ require_once('./controleur/Action.interface.php');
 require_once('./modele/TacheDAO.php');
 class EditTacheAction implements Action {
 	public function execute(){
-		return "listeActivites";
+		$dao = new TacheDAO();
+        if(isset($_REQUEST["titreEdit"]) and isset($_REQUEST["descriptionEdit"]) and isset($_REQUEST["dateEdit"])){//verifie que les champs sont remplis
+            return "listeActivites";
+        }
+        else{
+            $_REQUEST["action"]="affActivites";
+            return "listeActivites";   
+        }
 	}
 }
 ?>
