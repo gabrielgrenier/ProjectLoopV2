7e2c2cb56ed17ec9938654e762d2463071c9cfc2
GabrielGrenier <gabriel.braun-grenier@hotmail.com>
2018-05-12 16:07:45 -0400

    Ajout de alerte pour edit tache

--- controleur/ConfirmEditTache.php
+++ controleur/ConfirmEditTache.php
@@ -10,11 +10,26 @@ class ConfirmEditTache implements Action {
         $date = $_REQUEST['dateEdit'];
         $userAssigned = $_REQUEST['userAssignedEdit'];
         $tache = $dao->find($id);
-        $tache->setTitre($titre);
-        $tache->setDescription($description);
-        $tache->setDateFin($date);
-        $tache->setUserAssigned($userAssigned);
-        $dao->update($tache);
+        if($tache!=null){
+            if($tache->getNumProjet()==$_SESSION["numProjet"]){
+                $tache->setTitre($titre);
+                $tache->setDescription($description);
+                $tache->setDateFin($date);
+                $tache->setUserAssigned($userAssigned);
+                $dao->update($tache);
+                $_SESSION["alert"]->setType("posTache");
+                $_SESSION["alert"]->setMessage("La tâche a été modifiée.");
+
+            }
+            else{
+                $_SESSION["alert"]->setType("negTache");
+                $_SESSION["alert"]->setMessage("La tâche que vous tentez de modifier ne fait pas partie de ce projet.");
+            }
+        }
+        else{
+            $_SESSION["alert"]->setType("negTache");
+            $_SESSION["alert"]->setMessage("La tâche que vous tentez de modifier n'existe pas.");
+        }
 		return "listeActivites";
 	}
 }
