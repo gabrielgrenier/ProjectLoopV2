1b20b222ea75f3936689b7a27de8921cbc40203c
GabrielGrenier <gabriel.braun-grenier@hotmail.com>
2018-05-12 16:24:54 -0400

    Ajout alerte pour action de role user

--- controleur/SetModoAction.php
+++ controleur/SetModoAction.php
@@ -3,21 +3,43 @@ require_once('./controleur/Action.interface.php');
 require_once('./modele/ProjetDAO.php');
 class SetModoAction implements Action {
 	public function execute(){
+        $dao = new ProjetDAO();
+        $projetTemp = new Projets();
+        $projetTemp->setEmail($_SESSION["current_email"]);
+        $projetTemp->setNumProjet($_SESSION["numProjet"]);
+        
+        $proRole = $dao->findByProjet($projetTemp);
+        if($proRole!=null){
+            if($proRole->getRole()!="admin" or $proRole->getRole()!="modo"){
+                if(isset($_REQUEST["user"])&&isset($_REQUEST["projet"])){
+                    $projetTemp2 = new Projets();
+                    $projetTemp2->setEmail($_REQUEST["user"]);
+                    $projetTemp2->setNumProjet($_REQUEST["projet"]);
 
-        if(isset($_REQUEST["user"])&&isset($_REQUEST["projet"])){
-            $dao = new ProjetDAO();
-            $projetTemp = new Projets();
-            $projetTemp->setEmail($_REQUEST["user"]);
-            $projetTemp->setNumProjet($_REQUEST["projet"]);
-            
-            $projet = $dao->findByProjet($projetTemp);
-            if($projet!=null){
-                $projet->setRole("modo");
-                $dao->updateUser($projet);
-                return "listeActivites";
+
+                    $projet = $dao->findByProjet($projetTemp);
+                    if($projet!=null){
+                        $projet->setRole("modo");
+                        $dao->updateUser($projet);
+                        $_SESSION["alert"]->setType("posTache");
+                        $_SESSION["alert"]->setMessage("L'utilisateur est désormais un modo du projet.");
+                    }
+                    else{
+                        $_SESSION["alert"]->setType("negTache");
+                        $_SESSION["alert"]->setMessage("Vous devez être admin ou modo du projet pour faire ceci.");
+                    }
+                }   
             }
-            return "listeActivites";
+            else{
+                $_SESSION["alert"]->setType("negTache");
+                $_SESSION["alert"]->setMessage("Vous devez être admin ou modo du projet pour faire ceci.");
+            }
         }
+        else{
+            $_SESSION["alert"]->setType("negTache");
+            $_SESSION["alert"]->setMessage("Vous ne faites pas partit de ce projet.");
+        }
+        return "listeActivites";
 	}
 }
 ?>
