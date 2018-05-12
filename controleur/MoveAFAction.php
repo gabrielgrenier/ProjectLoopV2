ea84e1bcc0f493f4306e7ab49d43465db1b9f734
GabrielGrenier <gabriel.braun-grenier@hotmail.com>
2018-05-12 16:14:53 -0400

    Ajout d'alerte negative dans les moves actions

--- controleur/MoveAFAction.php
+++ controleur/MoveAFAction.php
@@ -4,11 +4,24 @@ require_once('./modele/TacheDAO.php');
 class MoveAFAction implements Action {
 	public function execute(){
         $dao = new TacheDAO();
+        date_default_timezone_set('America/Toronto');
         $id = $_REQUEST['id'];
         $tache = $dao->find($id);
-        if($tache!=null and $tache->getUserAssigned()==$_SESSION["current_user"]){
-            $tache->setStatut(1);
-            $dao->update($tache);
+        
+        if($tache!=null){
+            if($tache->getUserAssigned()==$_SESSION["current_user"]){
+                $tache->setStatut(1);
+                $tache->setDateDebut(date("Y/m/d"));
+                $dao->update($tache);
+            }
+            else{
+                $_SESSION["alert"]->setType("negTache");
+                $_SESSION["alert"]->setMessage("Vous ne disposez pas des droits necessaire pour déplacer cette tâche.");
+            }
+        }
+        else{
+            $_SESSION["alert"]->setType("negTache");
+            $_SESSION["alert"]->setMessage("la tache n'existe pas.");
         }
 		return "listeActivites";
 	}
